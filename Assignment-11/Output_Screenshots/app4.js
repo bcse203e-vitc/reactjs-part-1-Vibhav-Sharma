b61ef31e04c4708bcdf653a88fbdae8a9c221bce
Magan Hara <150593601+Vibhav-Sharma@users.noreply.github.com>
2025-02-25 15:00:02 +0530

    Create app4.js

---
+++ Assignment-11/Output_Screenshots/app4.js
@@ -0,0 +1,17 @@
+function Header() {
+ return <h1>Welcome to My Website</h1>;
+}
+function Footer() {
+ return <p>© 2025 My Website</p>;
+}
+
+
+
+export default function App(){
+    return (
+        <section>
+            <Header/>
+            <Footer/>
+        </section>
+    )
+}
