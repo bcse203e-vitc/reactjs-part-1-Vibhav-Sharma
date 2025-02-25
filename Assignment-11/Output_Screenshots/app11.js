b1962c011e718d39325ead12a9c451870115958f
Magan Hara <150593601+Vibhav-Sharma@users.noreply.github.com>
2025-02-25 15:11:21 +0530

    Create app11.js

---
+++ Assignment-11/Output_Screenshots/app11.js
@@ -0,0 +1,12 @@
+const users = ["Alice", "Bob", "Charlie"];
+function UserList() {
+ return (
+ <ul>
+ {users.map((user, index) => (
+ <li key={index}>{user}</li>
+ ))}
+ </ul>
+ );
+}
+
+export default UserList;
